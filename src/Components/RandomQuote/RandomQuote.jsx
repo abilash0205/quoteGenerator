de14e12037cf79f56f530b4d36695239e8c6895e
abilash0205 <abilashraman@gmail.com>
2023-09-23 19:53:06 +0530

    created the quote generator

---
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./RandomQuote.css";
+import reloadIcon from '../Assets/iconmonstr-synchronization-18.svg'
+import twitterIcon from '../Assets/iconmonstr-retweet-1.svg'
+
+const RandomQuote = () => {
+
+    let quotes = [];
+
+    async function loadQuotes(){
+        const response = await fetch("https://type.fit/api/quotes");
+        quotes = await response.json();
+    }
+
+    const random = () => {
+        const select = quotes[Math.floor(Math.random() * quotes.length)];
+        setQuote(select);
+    }
+
+  const [quote, setQuote] = useState({
+    text: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    author: "Albert Einstein",
+  });
+
+  const twitterShare = () => {
+    window.open(`https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author.split(',')[0]}`)
+  }
+
+  loadQuotes();
+
+  return (
+    <div className="container">
+      <div className="quote">{quote.text}</div>
+      <div>
+        <div className="line"></div>
+        <div className="bottom">
+          <div className="author">- {quote.author.split(',')[0]}</div>
+          <div className="icons">
+            <img 
+                src={reloadIcon} 
+                alt="reloadButton"
+                onClick={() => {random()}} 
+            />
+            <img 
+                src={twitterIcon} 
+                alt="shareButton"
+                onClick={() => {twitterShare()}}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RandomQuote;
